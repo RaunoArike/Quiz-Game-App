fcbdaf68ff57e8f4dcd92b50ab74827074f5ed3c
Rauno Arike <R.Arike@student.tudelft.nl>
2022-02-25 21:41:23 +0100

    add endpoint classes

--- server/src/main/java/server/api/QuestionsController.java
+++ server/src/main/java/server/api/QuestionsController.java
@@ -1,2 +1,19 @@
-package server.api;public class Questions {
+package server.api;
+
+import commons.Question;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class QuestionsController {
+
+    @GetMapping("api/questions")
+    public List<Question> getQuestions() {
+        return List.of(new Question("Estimation", "How much electricity does a computer consume in an hour?", "200 W"),
+                (new Question("Estimation", "How much electricity does a wind turbine generate in an hour?", "1 MW")));
+    }
 }
