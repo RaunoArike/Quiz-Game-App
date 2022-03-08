e2cf83f15f2d9505aaebee1ebe0735f3e5b05d24
Rauno Arike <R.Arike@student.tudelft.nl>
2022-03-08 23:37:38 +0100

    Add game controller

--- server/src/main/java/server/api/GameController.java
+++ server/src/main/java/server/api/GameController.java
@@ -1,34 +1,39 @@
 package server.api;
 
-import commons.model.Question;
+import commons.clientmessage.QuestionAnswerMessage;
+import commons.clientmessage.SinglePlayerGameStartMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import server.service.GameService;
 
-import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping
 public class GameController {
 
-    @MessageMapping("/question")
-    @SendToUser("")
-    public List getQuestion() {
-        Question question = QuestionServiceImpl.generateQuestion();
-        return List.of(question, 1);
+    private final GameService service;
+
+    public GameController(GameService service) {
+        this.service = service;
     }
 
+    @MessageMapping("/start-single-player")
+    public void startSinglePlayerGame(@Payload SinglePlayerGameStartMessage startMessage, SimpMessageHeaderAccessor headerAccessor) {
+        Map<String, Object> attrs = headerAccessor.getSessionAttributes();
+        attrs.put("player", service.startSinglePlayerGame(startMessage.getUsername()));
+    }
 
-    @MessageMapping("/answer")
-    public List<Integer> getAnswer() {
-        int answer = QuestionServiceImpl.generateAnswer(question.getActivities());
-        int questionScore = 0;
-        if (answer.equals(userAnswer)) {
-            questionScore = 1000 - timePassed * 0.01;
-            totalScore = totalScore + questionScore;
-        }
-        return 	List.of(questionScore, totalScore);
+    @MessageMapping("/score")
+    public void getScore(@Payload QuestionAnswerMessage answerMessage, SimpMessageHeaderAccessor headerAccessor) {
+        Map<String, Object> attrs = headerAccessor.getSessionAttributes();
+        int playerId = (Integer) attrs.get("player");
+        service.submitAnswer(playerId, answerMessage);
     }
 }
+
+
+
