436b051f20b18215540c910bbe0218bcbd7ff181
Rauno Arike <R.Arike@student.tudelft.nl>
2022-03-08 23:27:45 +0100

    Add interface for outgoing controller

--- server/src/main/java/server/api/OutgoingController.java
+++ server/src/main/java/server/api/OutgoingController.java
@@ -1,2 +1,25 @@
-package server.api;public interface OutgoingController {
+package server.api;
+
+import commons.servermessage.QuestionMessage;
+import commons.servermessage.ScoreMessage;
+
+import java.util.List;
+
+/**
+ * Controller for messages from server to client
+ */
+public interface OutgoingController {
+    /**
+     * Sends a new question to the client
+     * @param message - the message containing the question
+     * @param players - the players that receive the message
+     */
+    void sendQuestion(QuestionMessage message, List<Integer> players);
+
+    /**
+     * Sends the client's current score to the client
+     * @param message - the message containing the score
+     * @param players - the players that receive the message
+     */
+    void sendScore(ScoreMessage message, List<Integer> players);
 }
