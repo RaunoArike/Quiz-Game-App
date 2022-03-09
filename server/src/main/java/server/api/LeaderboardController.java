f039deed5c889ad3f735f7c0a5aca06c5753f937
Rauno Arike <R.Arike@student.tudelft.nl>
2022-03-09 14:35:20 +0100

    Add controller for the leaderboard

--- server/src/main/java/server/api/LeaderboardController.java
+++ server/src/main/java/server/api/LeaderboardController.java
@@ -1,2 +1,17 @@
-package server.api;public class LeaderboardController {
+
+package server.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class LeaderboardController {
+
+    @GetMapping("/api/leaderboard")
+    public String getLeaderboard() {
+        return null;
+    }
 }
