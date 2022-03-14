1bbe10b18f902d391f5094af83869d1da7bc56d5
Ye Zhao <Y.Zhao-33@student.tudelft.nl>
2022-03-14 08:48:44 +0100

    Revert "add modules for new screen"
    This reverts commit aefddd53d3d7f0ea26b7397d4abc0aa50e264750.

--- client/src/main/java/client/MyModule.java
+++ client/src/main/java/client/MyModule.java
@@ -21,8 +21,6 @@ import com.google.inject.Scopes;
 
 import client.scenes.OpeningCtrl;
 import client.scenes.MainCtrl;
-import client.scenes.EndingCtrl;
-import client.scenes.JoinWaitingroomCtrl;
 import client.scenes.LeaderboardCtrl;
 
 public class MyModule implements Module {
@@ -32,7 +30,5 @@ public class MyModule implements Module {
 		binder.bind(MainCtrl.class).in(Scopes.SINGLETON);
 		binder.bind(OpeningCtrl.class).in(Scopes.SINGLETON);
 		binder.bind(LeaderboardCtrl.class).in(Scopes.SINGLETON);
-		binder.bind(JoinWaitingroomCtrl.class).in(Scopes.SINGLETON);
-		binder.bind(EndingCtrl.class).in(Scopes.SINGLETON);
 	}
 }
