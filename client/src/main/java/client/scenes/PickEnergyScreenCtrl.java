1da488cfe56cc7a1313c461f2d4b72a808c2981e
Max Tanis <M.Tanis@student.tudelft.nl>
2022-03-15 15:43:56 +0100

    Added a new screen for estimation questions. Changed the name of the old estimation screen to PickEnergyScreen

--- client/src/main/java/client/scenes/PickEnergyScreenCtrl.java
+++ client/src/main/java/client/scenes/PickEnergyScreenCtrl.java
@@ -3,13 +3,13 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 
-public class EstimationScreenCtrl {
+public class PickEnergyScreenCtrl {
 
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
 
     @Inject
-    public EstimationScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
+    public PickEnergyScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
     }
