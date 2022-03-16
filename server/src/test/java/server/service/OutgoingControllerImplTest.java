55194a63c4005ca2c928e13f36b2021ea4e946b2
Rauno Arike <R.Arike@student.tudelft.nl>
2022-03-16 19:25:21 +0100

    Fix small errors

--- server/src/test/java/server/service/OutgoingControllerImplTest.java
+++ server/src/test/java/server/service/OutgoingControllerImplTest.java
@@ -1,2 +1,24 @@
-package server.service;public class OutgoingControllerTest {
+package server.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import server.api.OutgoingControllerImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class OutgoingControllerImplTest {
+
+	@Mock
+	private SimpMessagingTemplate template;
+
+	private OutgoingControllerImpl createController() {
+		return new OutgoingControllerImpl(template);
+	}
+
+	@Test
+	public void test_template_creation () {
+
+	}
 }
