4cb924d5f754f80f3b6922dd783c9825efc45d74
Ye Zhao <Y.Zhao-33@student.tudelft.nl>
2022-03-16 17:52:50 +0100

    Fixed Checkstyle for ServerService

--- client/src/main/java/client/service/ServerService.java
+++ client/src/main/java/client/service/ServerService.java
@@ -1,12 +1,12 @@
 package client.service;
 
 public interface ServerService {
-    //connect client to server
-    boolean connectToServer(String url);
+	//connect client to server
+	boolean connectToServer(String url);
 
-    //start a single player game;
-    void startSingleGame(String username);
+	//start a single player game;
+	void startSingleGame(String username);
 
-    //answer submit
-    void answerQuestions(Number answer);
+	//answer submit
+	void answerQuestions(Number answer);
 }
