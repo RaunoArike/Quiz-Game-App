25a9b01674e4ce16cbd29adb4fd556838b1a7763
Rauno Arike <R.Arike@student.tudelft.nl>
2022-03-17 15:53:17 +0100

    Add test for outgoing controller

--- server/src/test/java/server/service/OutgoingControllerImplTest.java
+++ server/src/test/java/server/service/OutgoingControllerImplTest.java
@@ -1,14 +1,23 @@
 package server.service;
 
+import commons.model.Activity;
+import commons.model.Question;
+import commons.servermessage.QuestionMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import server.api.OutgoingController;
 import server.api.OutgoingControllerImpl;
+
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class OutgoingControllerImplTest {
+	private static final Question FAKE_QUESTION = new Question.EstimationQuestion(new Activity("a", "b"), 4f);
 
 	@Mock
 	private SimpMessagingTemplate template;
@@ -19,6 +28,15 @@ public class OutgoingControllerImplTest {
 
 	@Test
 	public void test_template_creation() {
+		OutgoingController outgoingController = createController();
+		int playerId = 15;
+		String user = "15";
+		String dest = "/topic/question";
 
+		outgoingController.sendQuestion(
+				new QuestionMessage(FAKE_QUESTION, 0),
+				List.of(playerId));
+
+		verify(template).convertAndSendToUser(user, dest, new QuestionMessage(FAKE_QUESTION, 0));
 	}
 }
