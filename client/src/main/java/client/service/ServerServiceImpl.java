4bf0f803c4ac194abf55956ebd2f2f6f8d8c06b5
Ye Zhao <Y.Zhao-33@student.tudelft.nl>
2022-03-17 15:35:25 +0100

    Add interfaces implements

--- client/src/main/java/client/service/ServerServiceImpl.java
+++ client/src/main/java/client/service/ServerServiceImpl.java
@@ -1,5 +1,9 @@
 package client.service;
 
+import commons.clientmessage.QuestionAnswerMessage;
+import commons.clientmessage.SinglePlayerGameStartMessage;
+import commons.servermessage.QuestionMessage;
+import commons.servermessage.ScoreMessage;
 import org.springframework.lang.NonNull;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompHeaders;
@@ -9,12 +13,14 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 
 public class ServerServiceImpl implements ServerService {
 	private StompSession session;
-	private String dest;
+	private List<ServerListener> serverListeners = new ArrayList<>();
 
 	private StompSession connect(String url) {
 		var client = new StandardWebSocketClient();
@@ -31,8 +37,8 @@ public class ServerServiceImpl implements ServerService {
 		throw new IllegalStateException();
 	}
 
-	private <T> void registerForMessages(String d, Class<T> type, Consumer<T> consumer) {
-		session.subscribe(d, new StompSessionHandlerAdapter() {
+	private <T> void registerForMessages(String dest, Class<T> type, Consumer<T> consumer) {
+		session.subscribe(dest, new StompSessionHandlerAdapter() {
 
 			@Override
 			@NonNull
@@ -50,21 +56,34 @@ public class ServerServiceImpl implements ServerService {
 
 	@Override
 	public boolean connectToServer(String url) {
-		return false;
+		try {
+			session = connect(url);
+			registerForMessages("/topic/question", QuestionMessage.class, message -> {
+				serverListeners.forEach(serverListener -> serverListener.onQuestion(message));
+			});
+			registerForMessages("/topic/score", ScoreMessage.class, message -> {
+				serverListeners.forEach(serverListener -> serverListener.onScore(message));
+			});
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public void startSingleGame(String username) {
-
+		session.send("/app/start-single-player", new SinglePlayerGameStartMessage(username));
 	}
 
 	@Override
 	public void answerQuestions(Number answer) {
-
+		Integer answerInt = answer instanceof Integer ? (Integer) answer : null;
+		Float answerFloat = answer instanceof Float ? (Float) answer : null;
+		session.send("/app/submit-answer", new QuestionAnswerMessage(answerInt, answerFloat, 0));
 	}
 
 	@Override
 	public void registerListener(ServerListener serverListener) {
-
+		serverListeners.add(serverListener);
 	}
 }
