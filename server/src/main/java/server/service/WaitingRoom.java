e3df464ebcb5c875259f62f2c24813efdb95395b
MihneaHedea <i.m.hedea@student.tudelft.nl>
2022-03-20 12:50:33 +0100

    Revised the classes

--- server/src/main/java/server/service/WaitingRoom.java
+++ server/src/main/java/server/service/WaitingRoom.java
@@ -2,11 +2,22 @@ package server.service;
 
 
 public interface WaitingRoom {
+	/**
+	 *Check if the given player by their name is in the waiting room
+	 * @param playerName The name of the player
+	 * @return true if they are in the waiting room, false otherwise
+	 */
 	boolean isInWaitingRoom(String playerName);
 
-	void joinWaitingRoom(String palyerName);
+	/**
+	 *Puts the player in the waiting room if not already
+	 * @param playerName The name of the player
+	 */
+	void joinWaitingRoom(String playerName);
 
-	boolean isInProgress();
-
+	/**
+	 * The game of the given waiting room is started by one of the players
+	 * @param playerName The name of one player from the waiting room
+	 */
 	void startMultiplayerGame(String playerName);
 }
--- server/src/main/java/server/service/WaitingRoomImpl.java
+++ server/src/main/java/server/service/WaitingRoomImpl.java
@@ -4,43 +4,50 @@ import lombok.Getter;
 import lombok.Setter;
 import server.api.OutgoingController;
 import server.model.Game;
+import server.model.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WaitingRoomImpl implements WaitingRoom {
 	@Getter
 	@Setter
 	//Maps the playerId to the waiting room they belong
-	private Map<String, WaitingRoom> listOfPlayers = new HashMap<>();
-	// gameIds to game
-	private final Map<Integer, Game> games = new HashMap<>();
-	private final Map<Integer, Integer> playersToGame = new HashMap<>();
+	private Map<String, WaitingRoom> players = new HashMap<>();
+	private final List<Player> listOfPlayers;
+	private final OutgoingController outgoingController;
 	private int playersInWaitingRoom;
 	private Game currentGame;
-	private String gameId;
-	private final OutgoingController outgoingController;
+	private Integer gameId;
 	public WaitingRoomImpl(OutgoingController outgoingController) {
 		this.outgoingController = outgoingController;
+		listOfPlayers = new ArrayList<>();
 	}
 
 	@Override
 	public boolean isInWaitingRoom(String playerName) {
-		return false;
+		return players.containsKey(playerName) && listOfPlayers.contains(new Player(playerName));
 	}
 
 	@Override
 	public void joinWaitingRoom(String playerName) {
-
-	}
-
-	@Override
-	public boolean isInProgress() {
-		return false;
+		/*
+		 * If the player isn't already in the waiting room,
+		 * we add them to the map of players to the current waiting room
+		 * * and to the list of players.
+		 */
+		if (!isInWaitingRoom(playerName)) {
+			playersInWaitingRoom++;
+			Player currentPlayer = new Player(playerName);
+			players.put(playerName, this);
+			listOfPlayers.add(new Player(playerName));
+		}
 	}
 
 	@Override
 	public void startMultiplayerGame(String playerName) {
-
+		//TODO
 	}
 }
