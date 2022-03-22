4a810a17cc6f8d4641ab376c8bd2f5f1d7bafb9e
Karol Jurski <K.K.Jurski@student.tudelft.nl>
2022-03-22 14:05:04 +0100

    Fix record getter calls

--- client/src/main/java/client/service/MessageLogicService.java
+++ client/src/main/java/client/service/MessageLogicService.java
@@ -31,30 +31,30 @@ public class MessageLogicService implements ServerListener {
 	 * @param message message with new question details
 	 */
 	public void onQuestion(QuestionMessage message) {
-		int questionNumber = message.getQuestionNumber();
-		Question question = message.getQuestion();
+		int questionNumber = message.questionNumber();
+		Question question = message.question();
 		if (question instanceof ComparisonQuestion) {
 			ComparisonQuestion q = (ComparisonQuestion) question;
 			currentType = QuestionTypes.COMPARISON;
-			correctAnswer = q.getCorrectAnswer();
+			correctAnswer = q.correctAnswer();
 			mainCtrl.showComparisonQuestion(q, questionNumber, score);
 		}
 		if (question instanceof EstimationQuestion) {
 			EstimationQuestion q = (EstimationQuestion) question;
 			currentType = QuestionTypes.ESTIMATION;
-			correctAnswer = q.getCorrectAnswer();
+			correctAnswer = q.correctAnswer();
 			mainCtrl.showEstimationQuestion(q, questionNumber, score);
 		}
 		if (question instanceof MultiChoiceQuestion) {
 			MultiChoiceQuestion q = (MultiChoiceQuestion) question;
 			currentType = QuestionTypes.MULTI_CHOICE;
-			correctAnswer = q.getCorrectAnswer();
+			correctAnswer = q.correctAnswer();
 			mainCtrl.showMultiChoiceQuestion(q, questionNumber, score);
 		}
 		if (question instanceof PickEnergyQuestion) {
 			PickEnergyQuestion q = (PickEnergyQuestion) question;
 			currentType = QuestionTypes.PICK_ENERGY;
-			correctAnswer = q.getCorrectAnswer();
+			correctAnswer = q.correctAnswer();
 			mainCtrl.showPickEnergyQuestion(q, questionNumber, score);
 		}
 	}
@@ -64,8 +64,8 @@ public class MessageLogicService implements ServerListener {
 	 * @param message message with player's score
 	 */
 	public void onScore(ScoreMessage message) {
-		this.score = message.getTotalScore();
-		mainCtrl.showAnswer(currentType, correctAnswer, message.getQuestionScore());
+		this.score = message.totalScore();
+		mainCtrl.showAnswer(currentType, correctAnswer, message.questionScore());
 	}
 
 	/**
