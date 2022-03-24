85b39bbce21a9e9a3b5bc969673a2397a9b351f6
Karol Jurski <K.K.Jurski@student.tudelft.nl>
2022-03-24 13:16:57 +0100

    Fix broken timer

--- client/src/main/java/client/scenes/QuestionCtrl.java
+++ client/src/main/java/client/scenes/QuestionCtrl.java
@@ -13,9 +13,12 @@ import java.util.TimerTask;
 
 public abstract class QuestionCtrl {
 
+	private static final int TIMER_UPDATE_PERIOD = 1000;
+	private static final double TIMER_PROGRESS_PERCENTAGE = 0.05;
+	private static final double TIMER_SECONDS = 20;
+
 	protected final ServerService server;
 	protected final MainCtrl mainCtrl;
-	private static final int PERIOD = 1000;
 
 	@FXML
 	private Label questionText;
@@ -36,6 +39,7 @@ public abstract class QuestionCtrl {
 	private Text timerNumber;
 
 	private final Timer timer = new Timer();
+	private TimerTask timerTask;
 
 	@Inject
 	public QuestionCtrl(ServerService server, MainCtrl mainCtrl) {
@@ -53,28 +57,25 @@ public abstract class QuestionCtrl {
 	}
 
 	public void callTimeLimiter() {
-		TimerTask timerTask = new TimerTask() {
+		timerTask = new TimerTask() {
 			double timeLeft = 1;
 
 			@Override
 			public void run() {
-				final double progressPercentage = 0.05;
-				final double seconds = 20;
-
-				if (timeLeft >= progressPercentage) {
-					timeLeft -= progressPercentage;
+				if (timeLeft >= TIMER_PROGRESS_PERCENTAGE) {
+					timeLeft -= TIMER_PROGRESS_PERCENTAGE;
 					timerProgress.setProgress(timeLeft);
-					timerNumber.setText(String.valueOf(Math.round(timerProgress.getProgress() * seconds)));
+					timerNumber.setText(String.valueOf(Math.round(timerProgress.getProgress() * TIMER_SECONDS)));
 				} else {
 					timerNumber.setText("0");
 					timerProgress.setProgress(0);
 				}
 			}
 		};
-		timer.schedule(timerTask, 0, PERIOD);
+		timer.schedule(timerTask, 0, TIMER_UPDATE_PERIOD);
 	}
 
 	public void timeStop() {
-		timer.cancel();
+		timerTask.cancel();
 	}
 }
