afe6a12e52d1b6b2a1f2271556b445905bb32037
Rauno Arike <R.Arike@student.tudelft.nl>
2022-03-24 16:47:15 +0100

    Modify the tests

--- server/src/test/java/server/service/ActivityControllerTest.java
+++ server/src/test/java/server/service/ActivityControllerTest.java
@@ -6,25 +6,33 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import server.api.ActivityController;
+import server.entity.ActivityEntity;
 import server.exception.IdNotFoundException;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 public class ActivityControllerTest {
 
-	private static final List<Activity> FAKE_ACTIVITY_LIST = List.of(
-			new Activity("A1", null, 1f),
-			new Activity("A2", null, 2f),
-			new Activity("A3", null, 3f)
-	);
+	private static final Activity FAKE_ACTIVITY = new Activity("A4", null, 4f);
 
 	@Mock
 	private ActivityService activityService;
 
 	private ActivityController createController() {
 		return new ActivityController(activityService);
+	}
+
+	@Test
+	public void updateActivity_should_throw_exception_for_wrong_id() {
+		var controller = createController();
+		doThrow(new EntityNotFoundException()).when(activityService).updateActivity(150000, FAKE_ACTIVITY);
+
+		assertThrows(IdNotFoundException.class, () -> controller.updateActivity(150000, FAKE_ACTIVITY));
 	}
 }
--- server/src/test/java/server/service/ActivityServiceImplTest.java
+++ server/src/test/java/server/service/ActivityServiceImplTest.java
@@ -11,11 +11,13 @@ import server.entity.ActivityEntity;
 import server.exception.IdNotFoundException;
 import server.repository.ActivityRepository;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ActivityServiceImplTest {
@@ -24,6 +26,8 @@ public class ActivityServiceImplTest {
 			new Activity("A2", null, 2f),
 			new Activity("A3", null, 3f)
 	);
+	private static final List<ActivityEntity> FAKE_ACTIVITY_ENTITY_LIST = new ArrayList<>();
+	private static final Activity FAKE_UPDATED_ACTIVITY = new Activity("A4", null, 4f);
 
 	@Mock
 	private ActivityRepository activityRepository;
@@ -49,8 +53,27 @@ public class ActivityServiceImplTest {
 	}
 
 	@Test
-	public void provideActivities_should_return_a_list_of_all_activities_in_repo() {
+	public void provideActivities_should_return_a_list_containing_all_activities_in_repo() {
 		var service = createService();
+		for (Activity activity : FAKE_ACTIVITY_LIST) {
+			FAKE_ACTIVITY_ENTITY_LIST.add(ActivityEntity.fromModel(activity));
+		}
+
+		when(activityRepository.findAll()).thenReturn(FAKE_ACTIVITY_ENTITY_LIST);
+
+		assertEquals(service.provideActivities().size(), FAKE_ACTIVITY_LIST.size());
+	}
+
+	@Test
+	public void provideActivities_should_return_Activities_from_repo() {
+		var service = createService();
+		for (Activity activity : FAKE_ACTIVITY_LIST) {
+			FAKE_ACTIVITY_ENTITY_LIST.add(ActivityEntity.fromModel(activity));
+		}
+
+		when(activityRepository.findAll()).thenReturn(FAKE_ACTIVITY_ENTITY_LIST);
+
+		assertEquals(service.provideActivities().get(0), FAKE_ACTIVITY_LIST.get(0));
 	}
 
 	@Test
@@ -64,18 +87,15 @@ public class ActivityServiceImplTest {
 	@Test
 	public void updateActivity_should_update_the_contents_of_an_activity() {
 		var service = createService();
-	}
+		for (Activity activity : FAKE_ACTIVITY_LIST) {
+			FAKE_ACTIVITY_ENTITY_LIST.add(ActivityEntity.fromModel(activity));
+		}
 
-	@Test
-	public void updateActivity_should_update_the_activity_with_the_provided_id() {
-		var service = createService();
-	}
+		when(activityRepository.findAll()).thenReturn(FAKE_ACTIVITY_ENTITY_LIST);
+		when(activityRepository.getById(0L)).thenReturn(FAKE_ACTIVITY_ENTITY_LIST.get(0));
 
-	@Test
-	public void updateActivity_should_throw_exception_for_wrong_id() {
-		var controller = createService();
-		controller.addActivities(FAKE_ACTIVITY_LIST);
+		service.updateActivity(0, FAKE_UPDATED_ACTIVITY);
 
-		assertThrows(IdNotFoundException.class, () -> controller.updateActivity(150000, new Activity("A4", null, 4f)));
+		assertEquals(service.provideActivities().get(0), FAKE_UPDATED_ACTIVITY);
 	}
 }
