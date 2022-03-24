cdd7dae99aa73e3a2b634508c710b0b930d9c3db
MihneaHedea <i.m.hedea@student.tudelft.nl>
2022-03-24 11:28:48 +0100

    Changed: AdminCtrl.java

--- client/src/main/java/client/scenes/AdminCtrl.java
+++ client/src/main/java/client/scenes/AdminCtrl.java
@@ -7,29 +7,13 @@ import javafx.scene.input.KeyEvent;
 import javax.inject.Inject;
 import java.awt.*;
 
-import static javafx.scene.input.KeyCode.ENTER;
 
 public class AdminCtrl {
 	private final ServerUtils serverUtils;
 	private final MainCtrl mainCtrl;
 
-
-
-
-
-
-
-
-
 	@FXML
 	private  List listOfQuestions;
-
-
-
-
-
-
-
 
 	@Inject
 	public AdminCtrl(ServerUtils serverUtils, MainCtrl mainCtrl) {
@@ -38,10 +22,6 @@ public class AdminCtrl {
 		listOfQuestions = new List();
 	}
 
-
-
-
-
 	///The option to return home
 	public void returnHome() {
 		mainCtrl.showHome();
@@ -49,16 +29,8 @@ public class AdminCtrl {
 
 	public void keyPressed(KeyEvent keyEvent) {
 		switch (keyEvent.getCode()) {
-			case ENTER: break;
-			case ESCAPE: break;
+			case ESCAPE: returnHome(); break;
 			default: break;
 		}
 	}
-
-
-
-
-
-
-
 }
