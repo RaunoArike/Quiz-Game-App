c95356ab957f004cf4b24578945f473e9e08695c
Ye Zhao <Y.Zhao-33@student.tudelft.nl>
2022-03-25 09:38:48 +0100

    Adjust questionText and screens

--- client/src/main/java/client/scenes/QuestionCtrl.java
+++ client/src/main/java/client/scenes/QuestionCtrl.java
@@ -49,6 +49,7 @@ public abstract class QuestionCtrl {
 
 	public void setQuestion(String questionText) {
 		this.questionText.setText(questionText);
+		this.questionText.setWrapText(true);
 	}
 
 	public void setScore(int scoreNumber) {
