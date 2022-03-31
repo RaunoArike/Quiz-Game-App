99772b41a816bd7ebfecb1394cb15c28e47a2637
MihneaHedea <i.m.hedea@student.tudelft.nl>
2022-03-31 17:27:35 +0200

    INTERMEDIATE LEADERBOARD WORKS! Integrated the double points joker in the scoring system Changed: GameServiceImpl.java QuestionServiceImpl.java

--- client/src/main/java/client/scenes/IntermediateLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/IntermediateLeaderboardCtrl.java
@@ -7,7 +7,6 @@ import commons.model.LeaderboardEntry;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,7 +15,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 public class IntermediateLeaderboardCtrl extends AbstractCtrl implements Initializable {
@@ -47,9 +45,6 @@ public class IntermediateLeaderboardCtrl extends AbstractCtrl implements Initial
 	public IntermediateLeaderboardCtrl(ServerService server, MainCtrl mainCtrl) {
 		this.server = server;
 		this.mainCtrl = mainCtrl;
-
-		intermediateList = new ArrayList<>();
-
 	}
 
 	/**
@@ -74,10 +69,12 @@ public class IntermediateLeaderboardCtrl extends AbstractCtrl implements Initial
 	@Override
 	public void init() {
 		super.init();
-		ObservableList<LeaderboardEntry> observableList = FXCollections
+		/*ObservableList<LeaderboardEntry> observableList = FXCollections
 				.observableArrayList(intermediateList);
 
 		intermediaryLeaderboard.setItems(observableList);
+
+		 */
 	}
 
 }
